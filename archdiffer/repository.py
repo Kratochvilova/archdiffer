0e8b02d349667b146c7bf6d0c3fbbb2cbf0facb6
Pavla Kratochvilova <pavla.kratochvilova@gmail.com>
2018-05-07 15:53:32 +0200

    Added function for loading tests of plugins

--- archdiffer/repository.py
+++ archdiffer/repository.py
@@ -14,6 +14,7 @@ import os
 
 workers = {}
 flask_frontends = {}
+tests = {}
 
 def load_plugins_workers():
     """Load worker plugins from all plugins if possible."""
@@ -31,6 +32,16 @@ def load_plugins_flask_frontends():
         try:
             flask_frontends[name] = importlib.import_module(
                 '.plugins.' + name + '.flask_frontend', 'archdiffer'
+            )
+        except ImportError as ie:
+            print(ie)
+
+def load_plugins_tests():
+    """Load tests from all plugins if possible."""
+    for name in os.listdir(os.path.join(os.path.dirname(__file__), 'plugins')):
+        try:
+            tests[name] = importlib.import_module(
+                '.plugins.' + name + '.tests', 'archdiffer'
             )
         except ImportError as ie:
             print(ie)
