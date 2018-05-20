1cb71e7ad680d67d4c9132bbaff215c0a8d3152a
Pavla Kratochvilova <pavla.kratochvilova@gmail.com>
2018-05-20 16:02:17 +0200

    Docstrings

--- archdiffer/flask_frontend/config.py
+++ archdiffer/flask_frontend/config.py
@@ -11,7 +11,11 @@ Created on Fri Apr 14 22:18:26 2017
 
 from ..config import config
 
-def get_openid_providers():
+def openid_providers():
+    """Load dict of openid providers from config.
+
+    :return dict: dict of providers in format {'provider name': 'url'}
+    """
     providers = {}
     for section in config.sections():
         if section.startswith('openid'):
@@ -19,9 +23,10 @@ def get_openid_providers():
     return providers
 
 class FlaskConfig(object):
+    """Flask config created from the main config."""
     DEBUG = config['web']['DEBUG']
     SECRET_KEY = config['web']['SECRET_KEY']
     OPENID_FS_STORE_PATH = config['web']['OPENID_FS_STORE_PATH']
     API_TOKEN_LENGTH = int(config['web']['API_TOKEN_LENGTH'])
     API_TOKEN_EXPIRATION = int(config['web']['API_TOKEN_EXPIRATION'])
-    OPENID_PROVIDERS = get_openid_providers()
+    OPENID_PROVIDERS = openid_providers()
